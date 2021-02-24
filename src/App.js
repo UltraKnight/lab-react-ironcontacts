db729516234a761090eaea0774e3313289333b30
Vanderlei I Martins <vanderlei.i.martins@outlook.com>
2021-02-24 14:50:22 +0000

    done without components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import contacts from "./contacts.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    fiveFirst: contacts.slice(0, 5)
+  }
 
-export default App;
+  handleNewContact = () => {
+    this.setState(state => {
+      return {
+        fiveFirst: state.fiveFirst.concat(contacts[Math.floor(Math.random() * contacts.length)])
+      }
+    })
+  }
+  
+  handleSortByName = () => {
+    const newArr = [... this.state.fiveFirst];
+    this.setState({
+        fiveFirst: newArr.sort((a, b) => a.name.localeCompare(b.name))
+    })
+  }
+
+  handleSortByPopularity = () => {
+    const newArr = [... this.state.fiveFirst];
+    this.setState({
+        //dont access this.state here because the state can be being set yet
+        fiveFirst: newArr.fiveFirst.sort((a, b) => b.popularity - a.popularity)
+    })
+  }
+
+  handleDelete = (id) => {
+    this.setState(state => {
+      return {
+        fiveFirst: state.fiveFirst.filter(contact => contact.id !== id)
+      }
+    })
+  }
+
+  render() {
+    const {fiveFirst} = this.state;
+    return (
+    <div className="App">
+      <h1 style={{fontSize: '4rem'}}>IronContacts</h1>
+      <button onClick={this.handleNewContact}>Add Random Contact</button>
+      <button onClick={this.handleSortByName}>Sort by name</button>
+      <button onClick={this.handleSortByPopularity}>Sort by popularity</button>
+      <table style={{margin: '0 auto', fontSize: '2.5rem'}}>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody style={{textAlign: 'justify'}}>
+            {
+              fiveFirst.map(contact => {
+                return (
+                  <tr key={contact.id}>
+                    <td><img src={contact.pictureUrl} width="100px" /></td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
+                    <td><button onClick={this.handleDelete.bind(this, contact.id)}>Delete</button></td>
+                  </tr>
+                )
+              })
+            }
+        </tbody>
+      </table>
+    </div>
+  )};
+}
